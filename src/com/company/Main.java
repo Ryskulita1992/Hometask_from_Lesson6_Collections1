f921b86fe44d2f291f6bdd88842ff13a2810f3bf
admin <rsk.mva@gmail.com>
2020-02-02 17:00:32 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,70 @@
+package com.company;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> listA = new ArrayList<>(5);//Create a list of strings
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter  information");
+        listA.add(s.nextLine());
+        listA.add(s.nextLine());
+        listA.add(s.nextLine());
+        listA.add(s.nextLine());
+        listA.add(s.nextLine());
+        System.out.println(listA);
+
+        Iterator<String> iterator = listA.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        ArrayList<String> listB = new ArrayList<>(5);
+        System.out.println("Please enter information");
+        listB.add(s.nextLine());
+        listB.add(s.nextLine());
+        listB.add(s.nextLine());
+        listB.add(s.nextLine());
+        listB.add(s.nextLine());
+
+        Iterator<String> iterator2= listB.iterator();
+        while (iterator2.hasNext()) {
+            System.out.println(listB.iterator().next());
+
+        }
+        ArrayList<String>listC=new ArrayList<>();
+        Collections.sort(listA);
+        Collections.sort(listB);
+        Collections.sort(listB, Collections.reverseOrder());
+        //Collections.reverse(listB);
+
+        listC.addAll(listA);
+        listC.addAll(listB);
+        listC.toString().length();
+
+
+        System.out.println("List after the use of" +
+                " Collection.sort() :\n" + listC);
+    }
+        /* Collections.sort method is sorting the
+        elements of ArrayList in ascending order. */
+
+}
+
+
+
+
+
+
+
+
+
+
+
